0c54371ad5e0285cbf2569fd91fe436ab8af0290
Maulian_Santos <mauliansantoscruz@gmail.com>
2024-11-29 17:45:36 -0300

    adicionando contador de tempo para subir a API

--- biblioteca/manage.py
+++ biblioteca/manage.py
@@ -1,22 +1,18 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-import os
-import sys
+import time
+from django.core.management import execute_from_command_line
 
+if __name__ == "__main__":
+    import os
+    import sys
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'biblioteca.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "biblioteca.settings")
+    
+    # Capturar o tempo de início
+    start_time = time.time()
+
     try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
+        execute_from_command_line(sys.argv)
+    finally:
+        # Calcular o tempo de inicialização
+        elapsed_time = time.time() - start_time
+        print(f"Tempo de inicialização: {elapsed_time:.2f} segundos")
